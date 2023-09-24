d1c3496e1c614b5aaf35d48b4dff9a92b6b1db48
Aryan <aryanbiswari14@gmail.com>
2023-09-24 13:13:41 +0530

    add more style

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const sidebarRounds = ['default','elimination', 'passing', 'group', 'pictureA','
 
 // Modify questions array to include 'round' property for each question
 const questions = [
-    { round: 'default', format: 'text',  question: 'Welcome to this Quiz', answer: '' },
+    { round: 'default', format: 'text',  question: 'आपका स्वागत है', answer: '' },
     { round: 'elimination', format: 'text',  question: 'Q1  Krishna', answer: 'Radha' },
     { round: 'elimination', format: 'text',  question: 'Q2  भारत की सबसे बड़ी आवासीय इमारत कौन सी है', answer: 'राष्ट्रपति भवन' },
     { round: 'elimination', format: 'text',  question: 'Q3 भारत के प्रथम वित्त मंत्री कौन थे', answer: 'आर के शनमुखम चेट्टी' },
